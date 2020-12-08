4d4d2644f0c71e662918698c0a07672a9d15cc42
Leanna <lrlash17@ufl.edu>
2020-12-08 17:44:30 -0500

    added code to index.js

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import {header} from './modules/header';
+import {footer} from './modules/footer';
+import {lunch_generator} from './modules/lunch';
+
+class App {
+  constructor(){
+    this.renderTemplate();
+  }
+
+  renderTemplate(){
+
+  }
+}
+
+new App;
